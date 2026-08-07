916aed3cfb84a03cd24f988696277db69966927a
Mayank01510 <mayankkumawat01510@gmail.com>
2026-08-07 22:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (75.64%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
 
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color , int n ,int m , int start , int drow[] , int dcol[]){
-        image[sr][sc] = color;
+    void dfs(vector<vector<int>>& image,vector<vector<int>>& ans, int sr, int sc, int color , int n ,int m , int start , int drow[] , int dcol[]){
+        ans[sr][sc] = color;
 
         for(int i = 0;i<4;i++){
                 int dr = sr + drow[i];
                 int dc = sc + dcol[i];
                 if(dc >=0 && dr >=0 && dc < m && dr < n &&
-                image[dr][dc] == start ){
-                    dfs(image , dr , dc , color ,n,m,start , drow , dcol);
+                image[dr][dc] == start && ans[dr][dc] != color){
+                    dfs(image ,ans , dr , dc , color ,n,m,start , drow , dcol);
                 }
             }
     }
@@ -19,12 +19,13 @@ public:
         int m = image[0].size();
         int start = image[sr][sc];
 
-        if(start == color)return image ;
+        vector<vector<int>> ans = image;
 
         int drow[] = {1,0,-1,0};
         int dcol[] = {0,1,0,-1};
 
-        dfs(image , sr , sc , color ,n,m,start , drow , dcol);
-        return image;
+        dfs(image ,ans, sr , sc , color ,n,m,start , drow , dcol);
+        
+        return ans;
     }
 };
