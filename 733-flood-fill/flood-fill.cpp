4124c8ba3ec4ae488a1022df18d841caafe8e943
Mayank01510 <mayankkumawat01510@gmail.com>
2026-08-07 22:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (56.86%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
+
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color , int n ,int m , int start , int drow[] , int dcol[]){
+        image[sr][sc] = color;
+
+        for(int i = 0;i<4;i++){
+                int dr = sr + drow[i];
+                int dc = sc + dcol[i];
+                if(dc >=0 && dr >=0 && dc < m && dr < n &&
+                image[dr][dc] == start ){
+                    dfs(image , dr , dc , color ,n,m,start , drow , dcol);
+                }
+            }
+    }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        queue<pair<int,int>>q ;
-        q.push({sr,sc});
         int start = image[sr][sc];
-        image[sr][sc] = color;
 
         if(start == color)return image ;
 
         int drow[] = {1,0,-1,0};
         int dcol[] = {0,1,0,-1};
 
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-
-            for(int i = 0;i<4;i++){
-                int dr = r + drow[i];
-                int dc = c + dcol[i];
-                if(dc >=0 && dr >=0 && dc < m && dr < n &&
-                image[dr][dc] == start ){
-                    q.push({dr,dc});
-                    image[dr][dc] = color;
-                }
-            }
-        }
+        dfs(image , sr , sc , color ,n,m,start , drow , dcol);
         return image;
     }
 };
