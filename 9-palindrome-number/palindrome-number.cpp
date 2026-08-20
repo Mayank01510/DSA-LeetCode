ddcf4387db77ce5657133c22470f5cbf93e0891d
Mayank01510 <mayankkumawat01510@gmail.com>
2026-08-20 12:09:00 +0530

    Time: 7 ms (13.02%) | Memory: 11 MB (7.03%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int n = s.length();
+        int i = 0;
+        int j = n-1;
+        while(i<j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
