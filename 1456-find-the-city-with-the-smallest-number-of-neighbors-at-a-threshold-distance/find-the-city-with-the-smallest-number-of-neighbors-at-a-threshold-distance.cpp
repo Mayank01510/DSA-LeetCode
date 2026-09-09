c8756a62ce5e404b3265ba530ff95af7c3b7d000
Mayank01510 <mayankkumawat01510@gmail.com>
2026-09-09 22:15:32 +0530

    Time: 13 ms (66.52%) | Memory: 17.4 MB (29.50%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,73 +1,41 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
+        for(int i = 0;i<n;i++){
+            dis[i][i] = 0;
+        }
         
-        vector<vector<pair<int,int>>> adj(n);
-
-        // Build adjacency list
-        for(auto it : edges) {
+        for(int i = 0;i<edges.size();i++){
+            auto it = edges[i];
             int u = it[0];
             int v = it[1];
             int w = it[2];
 
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+            dis[u][v] = w;
+            dis[v][u] = w;
         }
-
+        for(int k = 0 ;k<n;k++){
+            for(int i = 0;i<n;i++){
+                for(int j = 0;j<n;j++){
+                    if(dis[i][k] != INT_MAX && dis[k][j] != INT_MAX)
+                    dis[i][j] = min(dis[i][j] , dis[i][k] + dis[k][j]);
+                }
+            }
+        }
+        
+        int mincount = INT_MAX;
         int city = -1;
-        int minCount = INT_MAX;
-
-        // Run Dijkstra from every city
-        for(int src = 0; src < n; src++) {
-
-            vector<long long> dist(n, LLONG_MAX);
-
-            priority_queue<
-                pair<long long,int>,
-                vector<pair<long long,int>>,
-                greater<pair<long long,int>>
-            > pq;
-
-            dist[src] = 0;
-            pq.push({0, src});
-
-            while(!pq.empty()) {
-
-                auto [d, node] = pq.top();
-                pq.pop();
-
-                // Ignore outdated entry
-                if(d > dist[node])
-                    continue;
-
-                for(auto [next, wt] : adj[node]) {
-
-                    if(d + wt < dist[next] &&
-                       d + wt <= distanceThreshold) {
-
-                        dist[next] = d + wt;
-                        pq.push({dist[next], next});
-                    }
-                }
-            }
-
-            // Count reachable cities
+        for(int i = 0;i<n;i++){
             int count = 0;
-
-            for(int i = 0; i < n; i++) {
-                if(i != src && dist[i] <= distanceThreshold) {
-                    count++;
-                }
+            for(int j = 0;j<n;j++){
+                if( i != j && dis[i][j] <= distanceThreshold)count++;
             }
-
-            // Smaller count is better
-            // If equal, larger city number is better
-            if(count <= minCount) {
-                minCount = count;
-                city = src;
+            if(count <= mincount){
+                mincount = count;
+                city = i;
             }
         }
-
         return city;
     }
 };
